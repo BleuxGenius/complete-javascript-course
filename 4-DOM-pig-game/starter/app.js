995bd69215a14bf9c84be230c454d3ca0bd0dc16
YasirHasn9 <hassanyasir223@gmail.com>
2020-02-12 09:48:39 -0800

    update the value of the player

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,43 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+let scores, roundScore, activePlayer, active;
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+// setter
+document.querySelector(".dice").style.display = "none";
+
+// set the values of the players to 0
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+// select the btn for the dice
+document.querySelector(".btn-roll").addEventListener("click", () => {
+  // 1. generate a random number
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. display the result
+  let diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  //   3. update the value of the roundScore
+  if (dice !== 0) {
+    roundScore += dice;
+    document.querySelector(".dice-" + activePlayer).textContent = roundScore;
+  } else {
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+    document.getElementById("current-0").textContent = 0;
+    document.getElementById("current-1").textContent = 0;
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+  }
+});
